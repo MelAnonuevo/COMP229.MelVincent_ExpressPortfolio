f8a9507aa205e7a7498f15c25b2e10832cd389a9
MelAnonuevo <97712598+MelAnonuevo@users.noreply.github.com>
2022-03-10 01:18:12 -0500

    Backend Update

---
+++ config/config.js
@@ -0,0 +1,9 @@
+// Author: Mel Vincent Anonuevo
+// Student ID: 301167069
+// Date: Feb 1, 2022
+
+// Do not use it in production
+module.exports = {
+    "SECRETKEY": "42A461333C7B6EA1B7C9C7C6EBCA5",
+    "ATLASDB": "mongodb+srv://dbadmin:5WwaSJObLquB5cKz@comp229-mel.bmpbg.mongodb.net/COMP229-002"
+}
